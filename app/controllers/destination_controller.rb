67c664d70df0dab1cdff2b08f09ed4c1dc352c19
Kevin <kcardoso@uncc.edu>
2017-04-03 15:25:20 +0000

    Add Login, Destination, and Welcome Controllers

---
+++ app/controllers/destination_controller.rb
@@ -0,0 +1,18 @@
+class DestinationController < ApplicationController
+  def select
+    
+  end
+  
+  #def createtrip
+    
+  #end 
+  
+  #def updatetrip
+  #end 
+  
+  #def deletetrip
+  #end 
+  
+  #def showresults
+  #end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'destination/results'
+
+  get 'destination/select'
+
+  get 'login/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
