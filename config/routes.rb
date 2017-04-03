adef0601176d5e4f78a4a794483a82e2c96d723b
Kevin <kcardoso@uncc.edu>
2017-04-03 16:11:25 +0000

    Remove login controll. Only Welcome controller and Destination Controller as a resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
-  get 'destination/results'
-
-  get 'destination/select'
-
-  get 'login/index'
+  
+  resources :destination #Added Destination controller as a resources (following CRUD convention)
+  
+  root 'welcome#index' #Point Welcome page as root page
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
