94237b345e599535daea09882ea5f4ab090ce795
Kevin <kcardoso@uncc.edu>
2017-05-06 17:50:05 +0000

    Updated HTML/CSS

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -26,6 +26,14 @@ class TripsController < ApplicationController
         end
     end
     
+    def destroy
+        @destination = Destination.find(params[:destination_id])
+        @trip = @destination.trips.find(params[:id])
+        @trip.destroy
+        redirect_to destination_path(@destination)
+    end
+    
+    
     private 
         def trip_params
             params.require(:trip).permit(:depart_date, :return_date, :depart_time, :return_time, :mode)
